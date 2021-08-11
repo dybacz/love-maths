8a08784cc75faee100d57b264dad49498b678692
dybacz <jdybacz@gmail.com>
2021-08-11 14:39:00 +0000

    Addition of division game

--- assests/js/script.js
+++ assests/js/script.js
@@ -39,7 +39,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else  {
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
+    }else  {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType} Aborting!`;
     }
@@ -81,7 +83,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else {
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    }else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -123,7 +127,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivideQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 * operand2 : operand2 * operand1;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2: operand1;
     document.getElementById('operator').textContent = "/";
 }
